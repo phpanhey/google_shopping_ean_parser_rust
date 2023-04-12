f30afa5ab18ad20fb73008048cac0014f336d96a
Philipp Panhey <philipp.panhey@gmail.com>
2023-04-12 22:18:22 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,10 @@ fn main() {
     let ean = &args[1];
     let url = format!("https://www.google.com/search?q={}&tbm=shop", ean);
     let compare_page_url = extract_compare_page_url(get_html(url).unwrap(), ean.clone()).unwrap();
-    let prices = extract_prices(get_html(compare_page_url).unwrap());
-    let price_calculation = calculate_price_calculation(prices.clone());
-
-    print!(
-        "{},{},{},{},",
-        ean, price_calculation.min, price_calculation.max, price_calculation.average
-    );
-    let mut sep = "";
-    for price in prices.iter() {
-        print!("{}{}", sep, price.item);
-        sep = ","
-    }
+    // println!("{}",get_html(compare_page_url).unwrap());
+    
+    let data_item = extract_data(get_html(compare_page_url).unwrap());
+    // println!("{},{}",ean, data_item.to_string());
 }
 
 fn get_html(url: String) -> Result<String, Error> {
@@ -43,38 +35,27 @@ fn extract_compare_page_url(html: String, ean: String) -> Result<String, String>
     return Err("no `compare_page_url` found".to_string());
 }
 
-fn extract_prices(html: String) -> Vec<Price> {
-    let mut prices: Vec<Price> = Vec::new();
+fn extract_data(html: String) -> DataItem {
+    let mut prices: Vec<Decimal> = Vec::new();
     let doc = Document::from(&html[..]);
     for node in doc.find(Attr("id", "online")) {
         for b_elem in node.find(Name("b")) {
-            prices.push(Price {
-                item: item_price(b_elem),
-                shipping: extract_shipping_price(b_elem),
-            });
+            prices.push(item_price(b_elem));
+        }
+
+        for link in node.find(Name("a")) {
+            println!("{}", link.text());
         }
     }
-
-    return prices;
-}
-
-fn extract_shipping_price(b_elem: select::node::Node) -> Decimal {
-    let value = b_elem
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .last_child()
-        .unwrap()
-        .text()
-        .chars()
-        .skip(1) // skip the "+" sign
-        .take_while(|&c| c != ' ') // take characters until a space is encountered
-        .collect::<String>()
-        .replace(",", ".")
-        .replace("€", "");
-
-    return Decimal::from_str_exact(value.trim()).unwrap();
+    let calculation = calculate_price_calculation(prices.clone());
+    return DataItem {
+        price_min: calculation.min,
+        price_max: calculation.max,
+        price_average: calculation.average,
+        name: "lorem".to_string(),
+        url: "lorem.de".to_string(),
+        prices: prices,
+    };
 }
 
 fn item_price(b_elem: select::node::Node) -> Decimal {
@@ -82,10 +63,8 @@ fn item_price(b_elem: select::node::Node) -> Decimal {
     return Decimal::from_str_exact(val.trim()).unwrap();
 }
 
-fn calculate_price_calculation(prices: Vec<Price>) -> PriceCalculation {
-    let mut items: Vec<Decimal> = prices.iter().map(|price| price.item).collect();
-    let _shippings: Vec<Decimal> = prices.iter().map(|price| price.shipping).collect();
-
+fn calculate_price_calculation(prices: Vec<Decimal>) -> PriceCalculation {
+    let mut items = prices;
     items.sort();
 
     let first = items.first().unwrap();
@@ -100,15 +79,34 @@ fn calculate_price_calculation(prices: Vec<Price>) -> PriceCalculation {
         average: average,
     };
 }
-
-#[derive(Clone)]
-struct Price {
-    item: Decimal,
-    shipping: Decimal,
-}
-
 struct PriceCalculation {
     min: Decimal,
     max: Decimal,
     average: Decimal,
+}
+struct DataItem {
+    name: String,
+    url: String,
+    price_min: Decimal,
+    price_max: Decimal,
+    price_average: Decimal,
+    prices: Vec<Decimal>,
+}
+
+impl DataItem {
+    fn to_string(&self) -> String {
+        format!(
+            "{},{},{},{},{},{}",
+            self.name,
+            self.url,
+            self.price_min,
+            self.price_max,
+            self.price_average,
+            self.prices
+                .iter()
+                .map(|p| p.to_string())
+                .collect::<Vec<String>>()
+                .join(",")
+        )
+    }
 }
